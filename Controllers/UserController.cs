caa29bc1908357f28f223339f8ec2497277aa608
gabrisak <gabrielapmel271@gmail.com>
2024-11-13 09:24:14 -0300

    atualização

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -34,5 +34,5 @@ public class UserController : Controller
 
 public class ApplicationDbContext
 {
-    public object Usuários { get; internal set; }
+    public object Usuários { get; set; }
 }
--- wwwroot/Data/ApplicationDbContext.cs
+++ wwwroot/Data/ApplicationDbContext.cs
@@ -4,5 +4,5 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Usuários> Usuários { get; set; }
 
-    public ApplicationDbContext() : base("Server=GABI\\SQL14;Database=BD_FAZENDA;Trusted_Connection=True;") { }
+    public ApplicationDbContext() : base("Data Source=GABI\\SQL14;Initial Catalog=BD_FAZENDA;Integrated Security=True;Encrypt=False;") { }
 }s
